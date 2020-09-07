814cdbaabc2a5e48fa85391ceb9a293d84a38ea6
company.Jp <JIAPU>
2020-09-07 17:10:00 +0800

    添加了一些注释，并且修改了乱码

--- StmtTool.h
+++ StmtTool.h
@@ -1,12 +1,13 @@
 #pragma once
 /*
 * @class CStmtTool
-* @brief CStmtTool������SQLite���ݿ���н�����һ��CStmtTool�������һ�����
-* @discussion ��д������Էſ�����Stmt��������ٹ�������unique_ptr����ɣ�����������
-* @see
+* @brief CStmtTool 用来操纵sqlite3_stmt对象
+* @discussion 一个CStmtTool对应一个sqlite3_stmt对象
+* @author Jp
 *
 */
 #include "sqlite3.h"
+//指定UTF-8或UTF-16编码创建的数据库
 enum ENCODE
 {
 	UTF8,
@@ -14,19 +15,25 @@ enum ENCODE
 };
 class CStmtTool {
 private:
-	int m_RetCode;//���ִ�����ķ��ؽ��
+	/*标识一次sqlite3函数关于此sqlite3_stmt的执行状态，用户取得一次执行结果后，应当保存此结果（如果希望以后使用此结果的话，否则将会丢失此结果）*/
+	int m_RetCode;
 	char * m_StmtStr;
-	int m_size;//ԭʼ�ַ�����ռ�õ��ֽڴ�С
+	int m_size;
 	sqlite3_stmt * m_Stmt;
 protected:
 	void finalize();
 public:
 	int GetRetCode();
-	CStmtTool(sqlite3* db, const char * str, int strSize, ENCODE) noexcept;
+	CStmtTool(sqlite3* db, const char * str, int strSize, ENCODE code) noexcept;
 	~CStmtTool();
+	/*执行*/
 	void  step();
-	void changeStmt(sqlite3 * db,const char* str, int strSize, ENCODE);
-	const unsigned char * column_text(int icol);
-	int column_int(int icol);
+	/*执行*/
 	void operator() ();
+	/*改变此CStmtTool对象对应的SQL语句*/
+	void changeStmt(sqlite3 * db,const char* str, int strSize, ENCODE);
+	/*取得一个text结果，以UTF-8的形式返回*/
+	const unsigned char * column_text(int icol);
+	/*取得一个int结果*/
+	int column_int(int icol);
 };
