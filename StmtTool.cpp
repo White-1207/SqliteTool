239c23be41697399b1b97d05c045e57e3ada608f
company.Jp <JIAPU>
2020-09-10 11:19:49 +0800

    添加了对字符串的编解码
    CStmtTool类将column_text改为column_text_ANSI，用于将UTF8/UTF16/UTF32编码的字符串
    转换为ANSI编码（目前UTF16/UF32）尚未支持。
    CStmtTool类添加了bind_text_ANSI,用于将ANSI编码的字符串，转为UTF8/UTF16/UTF32编码
    的字符串，然后绑定到sqlite3_stmt上。

--- StmtTool.cpp
+++ StmtTool.cpp
@@ -19,11 +19,6 @@ CStmtTool::CStmtTool(sqlite3* db, const char * str, int strSize, ENCODE code) no
 		m_RetCode = sqlite3_prepare16_v2(db, m_StmtStr, sizeof(m_StmtStr), &m_Stmt, NULL);
 	}
 }
-CStmtTool::CStmtTool() noexcept
-	:m_size(0),m_Stmt(nullptr),m_StmtStr(nullptr),m_RetCode(-1)
-{
-	
-}
 
 void  CStmtTool::step() {
 	m_RetCode = sqlite3_step(m_Stmt);
@@ -38,17 +33,28 @@ void CStmtTool::finalize() {
 		delete[] m_StmtStr;
 	}
 	m_StmtStr = nullptr;
-	m_size = 0;
-	m_RetCode = -1;
 }
 CStmtTool::~CStmtTool() {
 	finalize();
 }
 
-const unsigned char* CStmtTool::column_text(int icol) {
-	if (m_Stmt != nullptr) {
-		return sqlite3_column_text(m_Stmt, icol);
+const unsigned char* CStmtTool::column_text_ANSI(int icol,ENCODE type) {
+	char * desMutliAnsi = nullptr;
+	int numAnsi = 0;
+	int sizeUtf8 = 1;//sqlite3_column_text 返回的字符串即使是空字符串，也是由0结尾的，因此，其返回值长度至少为1 
+	//语句不存在，返回nullptr;
+	if (m_Stmt == nullptr) {
+		return nullptr;
 	}
+	if (type == UTF8) {
+		const char * socMutliUtf8 = (const char *)sqlite3_column_text(m_Stmt, icol);
+		for (int i = 0; socMutliUtf8[i] != 0; i++) {
+			sizeUtf8++;
+		}
+		numAnsi = code.MutliToMutli(socMutliUtf8, sizeUtf8, CP_UTF8, CP_ACP, desMutliAnsi);
+		return (const unsigned char*)desMutliAnsi;
+	}
+	//暂时未定义utf16和utf32
 	return nullptr;
 }
 
@@ -60,7 +66,7 @@ int CStmtTool::column_int(int icol) {
 	return 0;
 }
 
-void CStmtTool::changeStmt(sqlite3 * db,const char* str, int strSize, ENCODE code) {
+void CStmtTool::changeStmt(sqlite3 * db, const char* str, int strSize, ENCODE code) {
 	finalize();
 	if (db == nullptr) {
 		m_RetCode = SQLITE_ERROR;
@@ -82,23 +88,40 @@ void CStmtTool::operator()() {
 	step();
 }
 
-int CStmtTool::GetRetCode(){
+int CStmtTool::GetRetCode() {
 	int tmp = m_RetCode;
 	m_RetCode = -1;//赋值为-1，是由于SQLITE3 C++API的宏定义都是大于等于0的
-	return tmp; 
+	return tmp;
 }
 
-bool CStmtTool::isOK(int sqliteVal){
-	if(GetRetCode()==sqliteVal){
+bool CStmtTool::isOK(int sqliteVal) {
+	if (GetRetCode() == sqliteVal) {
 		return true;
 	}
 	return false;
 }
 
-bool CStmtTool::operator==(int sqliteVal){
+bool CStmtTool::operator==(int sqliteVal) {
 	return isOK(sqliteVal);
 }
 
-bool CStmtTool::operator!=(int sqliteVal){
+bool CStmtTool::operator!=(int sqliteVal) {
 	return !isOK(sqliteVal);
+}
+
+void CStmtTool::bind_text_ANSI(int index, const char *str, int size, void(*func)(void*),ENCODE type) {
+	char * desStr = nullptr;
+	int num = 0;
+	UINT desCodePage = 0;
+	if (type == UTF8) {
+		num = code.MutliToMutli(str, size, CP_ACP, CP_UTF8, desStr);
+	}
+	if (num == 0) {
+		m_RetCode = -1;
+		return;
+	}
+	if (m_Stmt == nullptr) {
+		m_RetCode = -1;
+	}
+	m_RetCode = sqlite3_bind_text(m_Stmt, index, desStr, num, func);
 }
--- StmtTool.h
+++ StmtTool.h
@@ -1,7 +1,8 @@
 #pragma once
 
 #include "include\sqlite3.h"
-/* 
+#include "code.h"
+/*
 * @brief 指定UTF-8或UTF-16编码创建的数据库
 */
 enum ENCODE
@@ -19,18 +20,18 @@ enum ENCODE
 */
 class CStmtTool {
 private:
-	/*标识一次sqlite3函数关于此sqlite3_stmt的执行状态，用户取得一次执行结果后，应当保存此结果（如果希望以后使用此结果的话，否则将会丢失此结果）*/
+	/* @breif 标识一次sqlite3函数关于此sqlite3_stmt的执行状态，用户取得一次执行结果后，
+	*  应当保存此结果（如果希望以后使用此结果的话，否则将会丢失此结果）
+	*/
 	int m_RetCode;
 	char * m_StmtStr;
 	int m_size;
 	sqlite3_stmt * m_Stmt;
+	CCode code;
 protected:
 	void finalize();
 public:
 	int GetRetCode();
-	/*构造一个空的CStmtTool 对象*/
-	CStmtTool() noexcept;
-	/*构造一个立即可以使用的CStmtTool对象*/
 	CStmtTool(sqlite3* db, const char * str, int strSize, ENCODE code) noexcept;
 	~CStmtTool();
 	/*执行*/
@@ -38,14 +39,19 @@ public:
 	/*执行*/
 	void operator() ();
 	/*改变此CStmtTool对象对应的SQL语句*/
-	void changeStmt(sqlite3 * db,const char* str, int strSize, ENCODE);
-	/*取得一个text结果，以UTF-8的形式返回*/
-	const unsigned char * column_text(int icol);
+	void changeStmt(sqlite3 * db, const char* str, int strSize, ENCODE);
+	/*
+	* @brief 绑定参数
+	* @param socCodePage 指定
+	*/
+	void bind_text_ANSI(int index, const char *, int size, void(*)(void*),ENCODE type);
+	const unsigned char * column_text_ANSI(int icol,ENCODE type);
 	/*取得一个int结果*/
 	int column_int(int icol);
-	
-	/*查看返回Code*/
+	/*执行是否成功 */
 	bool isOK(int sqliteVal);
+	/*执行是否成功*/
 	bool operator==(int sqliteVal);
+	/*执行是否成功*/
 	bool operator!=(int sqliteVal);
 };
